f682a5bf5f36cee5cc90725fa368c43a90879e9d
Reichel-eze <reicheleze@gmail.com>
2024-08-14 22:02:27 -0300

    Terminado todo, solo que no entendi el 5)

--- programa.pl
+++ programa.pl
@@ -12,6 +12,11 @@ tiene(martin, compro(piru, 2010)).
 tiene(constanza, regalo(abril, 2006)).
 tiene(constanza, adopto(mambo, 2015)).
 
+%agrego esto para el ejercicio 7)
+tiene(constanza, compro(eze, 2002)).
+tiene(constanza, compro(quiel, 2002)).
+tiene(constanza, compro(rei, 2002)).
+
 tiene(hector, adopto(abril, 2015)).
 tiene(hector, adopto(mambo, 2015)).
 tiene(hector, adopto(buenaventura, 1971)).
@@ -89,16 +94,108 @@ tieneMasDeUnGato(Persona) :-
 
 puedeDormir(Persona) :-
     tieneMascota(Persona, _, _),
-    not((tieneMascota(Persona, Mascota, _), estaChapita(Mascota))).
+    not((tieneMascota(Persona, Mascota, _), mascota(Mascota, Animal), estaChapita(Animal))).
 
-estaChapita(Mascota) :- mascota(Mascota, perro(chico)).
-estaChapita(Mascota) :- mascota(Mascota, tortuga(_)).
+puedeDormirFORALL(Persona) :-
+    tieneMascota(Persona, _, _),
+    forall((tieneMascota(Persona, Mascota, _), mascota(Mascota, Animal)), not(estaChapita(Animal))).
 
-estaChapita(Mascota) :- 
-    mascota(Mascota, gato(macho, CantidadAcariciadas)),
-    CantidadAcariciadas < 10.
+%estaChapita(Mascota) :- mascota(Mascota, perro(chico)).
+%estaChapita(Mascota) :- mascota(Mascota, tortuga(_)).
 
+%estaChapita(Mascota) :- 
+%    mascota(Mascota, gato(macho, CantidadAcariciadas)),
+%    CantidadAcariciadas < 10.
 
+estaChapita(perro(chico)).
+estaChapita(tortuga(_)).
+estaChapita(gato(macho, CantidadMimos)) :- CantidadMimos < 10.
 
+/* 5)
+A veces las personas siguen llevando mascotas a sus casas a pesar de
+tener mascotas chapitas. En algunos casos esto genera crisis nerviosas...
+a) crisisNerviosa/2 es cierto para una persona y un año cuando, 
+el año anterior obtuvo una mascota que está chapita y ya antes 
+tenía otra mascota que está chapita. 
+Por ejemplo, en el 2008 Hector tuvo una crisis nerviosa 
+(ya tenía a buenaventura, que es chapita, y en el 2007 
+adoptó a severino... Resultado: en el 2008 tuvo una crisis nerviosa).
+b) Decir si es inversible (si se pueden hacer preguntas existenciales) 
+y por qué.
+*/
 
+/*
+crisisNerviosa(Persona, AnioCrisis) :-
+    AnioAnterior is AnioCrisis - 1,
+    tieneMascota(Persona, Mascota, AnioAnterior),
+    estaChapita(Mascota),
+    AnioAntes < AnioAnterior,
+    tieneMascota(Persona, OtraMascota, AnioAntes),
+    estaChapita(OtraMascota),
+    Mascota \= OtraMascota.
+*/
 
+crisisNerviosa(Persona, AnioCrisis) :-
+    tieneMascota(Persona, _, Anio),
+    adoptoChapitaAnioAnterior(Persona, Anio, AnioAnterior),
+    adoptoChapitaAnioPrevio(Persona, AnioAnterior),
+    AnioCrisis is Anio + 1.
+
+adoptoChapitaAnioAnterior(Persona, Anio, AnioAnterior) :-
+    AnioAnterior is Anio - 1,
+    tieneMascota(Persona, Mascota, AnioAnterior),
+    mascota(Mascota, Animal),
+    estaChapita(Animal).
+
+adoptoChapitaAnioPrevio(Persona, AnioAnterior) :-
+    tieneMascota(Persona, Mascota, AnioPrevio),
+    AnioPrevio < AnioAnterior,
+    mascota(Mascota, Animal),
+    estaChapita(Animal).
+
+% 6) mascotaAlfa/2 Relaciona una persona con el nombre de una mascota, 
+% cuando esa mascota domina al resto de las mascotas de esa persona. 
+% Se sabe que un gato siempre domina a un perro, que un perro grande
+% domina a uno chico, que un gato chapita domina a gatos no chapita, 
+% y que una tortuga agresiva domina cualquier cosa. 
+% Por ejemplo, kali es mascota alfa de Martín.
+
+mascotaAlfa(Persona, MascotaAlfa) :-
+    tieneMascota(Persona, MascotaAlfa, _),
+    mascota(MascotaAlfa, AnimalAlfa),
+    forall((tieneMascota(Persona, OtraMascota, _), mascota(OtraMascota, Animal),
+    MascotaAlfa \= OtraMascota), dominaA(AnimalAlfa, Animal)).
+
+dominaA(gato(_, _), perro(_)).
+dominaA(perro(grande), perro(chico)).
+
+dominaA(GatoChapita, GatoNoChapita) :-
+    estaChapita(GatoChapita), not(estaChapita(GatoNoChapita)).
+
+dominaA(tortuga(agresiva), _).
+
+% 7) materialista/1 se cumple para una persona cuando no tiene 
+% mascotas o compró más de las que adoptó. Hacer que sea inversible. 
+% Por ejemplo, si Constanza comprara 3 mascotas sería materialista 
+% (porque ya tiene 1 adoptada).
+
+materialista(Persona) :-
+    not(tieneMascota(Persona, _, _)).
+
+materialista(Persona) :-
+    cantidadCompradas(Persona, Compradas),
+    cantidadAdoptadas(Persona, Adoptadas),
+    Compradas > Adoptadas.
+
+cantidadCompradas(Persona, Cantidad) :-
+    tiene(Persona, _),
+    findall(Mascota, tiene(Persona, compro(Mascota, _)), Mascotas),
+    length(Mascotas, Cantidad).
+
+cantidadAdoptadas(Persona, Cantidad) :-
+    tiene(Persona, _),
+    findall(Mascota, tiene(Persona, adopto(Mascota, _)), Mascotas),
+    length(Mascotas, Cantidad).
+    
+    
+    
